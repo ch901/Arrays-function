7d326a5b6ea5a5a5c14004a770cfad617293347a
Chinwe Okpala <chinweamara34@gmail.com>
2023-03-08 23:22:26 -0500

    updates

--- index.js
+++ index.js
@@ -16,26 +16,53 @@
 // Array.prototype.flat()
 
 // qUESTION2
-let languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
+// let languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python']
 
-languages.push('Kotlin')
-console.log(languages)
+// languages.push('Kotlin')
+// console.log(languages)
 
-languages.splice(3, 0, 'Java')
-console.log(languages)
+// languages.splice(3, 0, 'Java')
+// console.log(languages)
 
-languages.shift('C#')
-console.log(languages)
+// languages.shift('C#')
+// console.log(languages)
 
-languages.unshift('Scala', 'Swift')
-console.log(languages)
+// languages.unshift('Scala', 'Swift')
+// console.log(languages)
 
-let removeElement = languages.splice(5, 1, 'Go', 'Rust')
-console.log(languages)
+// let removeElement = languages.splice(5, 1, 'Go', 'Rust')
+// console.log(languages)
 
 // Question3
 
+// let fruit = ['apple', 'mango', 'banana'];
+// 		function changeFruit( fruit ) {
+//     			fruit[2] = "orange";
+//     			return fruit;
+// 		}
+// console.log(fruit)
 
+changeFruit=[‘apple’, ‘mango’, ‘orange’] in computer counting start from. 0
+
+// question4
+
+Solution 1
+const getMaxFromArray = (array) => array.sort()[1]
+
+Solution 2
+const getMaxFromArray = (array) => {
+  let currentMaxNumber = 0;
+  
+  array.forEach((n) => {
+    if(n > currentMaxNumber){
+      currentMaxNumber = n
+    }
+  })
+  
+  return currentMaxNumber
+}
+
+console.log('getMaxFromArray', getMaxFromArray([4, 5, 10, -2]))
 
 
 
